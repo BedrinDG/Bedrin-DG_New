8ea572dce68954a17ff046e2b595d109de605452
Bedrin DG <bebedanila12@gmail.com>
2022-10-11 22:00:42 +0300

    Фото добавил

--- 2 занятие/Задание 1.py
+++ 2 занятие/Задание 1.py
@@ -6,4 +6,4 @@ z = 0.16 * 10**2
 s = (1 + (math.sin(x + y))**2)/abs(x - ((2*y)/(1+(x**2)*(y**2))))* x**abs(y)+(math.cos(math.atan(1/z)))**2
 print("Вариант 3")
 print(s)
-input("Нажмите Enter чтобы выйти")
+
